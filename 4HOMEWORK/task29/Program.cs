4f8b00487bb156f43dc0992b8fc90c18e92e01b1
Kamilya <kami130531@gmail.com>
2022-12-09 22:42:00 +0300

    task29

---
+++ 4HOMEWORK/task29/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+void Massiv(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(1000);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int num = ReadNumber("Введите число:");
+Massiv(num);

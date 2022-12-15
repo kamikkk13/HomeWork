69798a070226c0c84b5291baadbea558f79caa7e
Kamilya <kami130531@gmail.com>
2022-12-15 22:36:34 +0300

    6HOMEWORK/

---
+++ 6HOMEWORK/task41/Program.cs
@@ -0,0 +1,17 @@
+﻿void Number()
+{
+    bool result = true;
+    int count = 0;
+
+    while (result)
+    {
+        string num = Console.ReadLine();
+
+        if (num == "")
+            result = false;
+        else if (int.Parse(num) > 0)
+            count++;
+    }
+    Console.WriteLine($"Количество чисел больше нуля равно {count}");
+}
+Number();

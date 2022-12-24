c9f625a99d74e54cfa3dcbcc684fc2256737551e
Kamilya <kami130531@gmail.com>
2022-12-24 22:51:08 +0300

    8HOMEWORK/

---
+++ 8HOMEWORK/task54/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+int[,] SelectionSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+        
+            for (int k = j + 1; k < array.GetLength(1); k++)
+                if (array[i, maxPosition] < array[i,k]) maxPosition = k;
+            
+            (array[i,j],array[i, maxPosition]) = (array[i, maxPosition],array[i,j]);
+        }
+    }   
+    return array;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter range of numbers:");
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[,] arr_sort = SelectionSort(arr_1);
+Print(arr_sort);

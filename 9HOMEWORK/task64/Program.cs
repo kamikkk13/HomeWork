7f9d5723cc524821b55c6cb93640f31b49cafd2d
Kamilya <kami130531@gmail.com>
2022-12-24 23:40:05 +0300

    9h

---
+++ 9HOMEWORK/task64/Program.cs
@@ -0,0 +1,17 @@
+﻿string rec(int n)
+{
+    if (n == 2)
+        return "2 ";
+    if (n % 2  == 1)
+    {
+        n = n - 1;
+        return rec(n - 2) + $"{n} ";
+    }
+    else return rec(n - 2) + $"{n} ";
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(n));
---
+++ 9HOMEWORK/task68/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int rec(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return rec(m - 1, 1);
+    if (m > 0 && n > 0) return rec(m - 1, rec(m, n - 1));
+    return rec(m,n);
+}
+
+Console.WriteLine($"A({m},{n}) = {rec(m, n)}");
